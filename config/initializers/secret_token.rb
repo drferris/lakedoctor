cdf368861d12cade79f4a06213af0841baad0f5d
Dave Ferris <dave@entrision.com>
2013-06-24 15:34:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lakedoctor::Application.config.secret_token = '28d8c6911804165a362b3bda1f7d59fe47ab9f0c156362ab606cf42d5ad4600b2c72cc05c20bf23f3c50bdc3216ebb960ea6221e49c300b181e0c8146227175a'
